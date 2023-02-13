af61a31c30c47ea836392b94d54b449f06e86c0c
unknown <malekabbes665@gmail.com>
2023-02-13 10:15:31 +0100

    Added custom events filter for participants :rocket:

--- Event/admin.py
+++ Event/admin.py
@@ -25,14 +25,33 @@ class DateListFilter(admin.SimpleListFilter):
             return queryset.filter(event_date__gt=datetime.now())
         if self.value()=='Today Events':
             return queryset.filter(event_date__exact=datetime.now())
-
+class ParticipantsFilter(admin.SimpleListFilter):
+    title="Participants Number"
+    parameter_name='nbe_participant'
+    def lookups(self,request,model_admin):
+        return(
+            ('Superieur a 2',('Superieur a 2')),
+            ('Superieur a 5',('Superieur a 5')),
+            ('Superieur a 10',('Superieur a 10')),
+            ('Pas de participants',('Pas de participants'))
+        )
+    def queryset(self, request, queryset):
+        if self.value()=='Superieur a 2':
+            return queryset.filter(nbe_participant__gte=2)
+        if self.value()=='Superieur a 5':
+            return queryset.filter(nbe_participant__gte=5)
+        if self.value()=='Superieur a 10':
+            return queryset.filter(nbe_participant__gte=10)
+        if self.value()=='Pas de participants':
+            return queryset.filter(nbe_participant__exact=0)
+        
 
 @admin.register(Event) 
 class EventAdmin(admin.ModelAdmin):
     list_display=('title','description','nbe_participant','state','event_date','update_date','organizer')
 # admin.site.register(Event,EventAdmin)
     list_per_page=2
-    list_filter=('title',DateListFilter,'nbe_participant')
+    list_filter=('title',DateListFilter,ParticipantsFilter)
     fieldsets=[
         ('A propos', {
         "fields":(
