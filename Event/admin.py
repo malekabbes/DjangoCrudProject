7a30fe5cd6e5ec538d9a0c0cb007df30dc1a0551
unknown <malekabbes665@gmail.com>
2023-02-13 10:00:13 +0100

    Added custom events filter for date :rocket:

--- Event/admin.py
+++ Event/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from.models import *
+from datetime import datetime
 # Register your models here.
 
 class ParticipationAdmin(admin.TabularInline):
@@ -7,13 +8,31 @@ class ParticipationAdmin(admin.TabularInline):
     extra=1
 
 
+class DateListFilter(admin.SimpleListFilter):
+    title="Event Date"
+    parameter_name='event_date'
+    def lookups(self,request,model_admin):
+        return(
+            ('Past Event',('Past Event')),
+            ('Incoming Events',('Incoming Events')),
+            ('Today Events',('Today Events'))
+            # key value
+        )
+    def queryset(self, request, queryset):
+        if self.value()=='Past Event':
+            return queryset.filter(event_date__lt=datetime.now())
+        if self.value()=='Incoming Events':
+            return queryset.filter(event_date__gt=datetime.now())
+        if self.value()=='Today Events':
+            return queryset.filter(event_date__exact=datetime.now())
+
 
 @admin.register(Event) 
 class EventAdmin(admin.ModelAdmin):
     list_display=('title','description','nbe_participant','state','event_date','update_date','organizer')
 # admin.site.register(Event,EventAdmin)
     list_per_page=2
-    list_filter=('title','event_date','nbe_participant')
+    list_filter=('title',DateListFilter,'nbe_participant')
     fieldsets=[
         ('A propos', {
         "fields":(
